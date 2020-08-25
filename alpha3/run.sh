af4164d43c75c3d36971eac86f1c6898757ca869
Bari Mehdi77 <mbari@student.1337.ma>
2020-08-25 16:01:38 +0100

    add sprite. but need more work

--- alpha3/run.sh
+++
@@ -1,6 +0,0 @@
-#gcc main.c ft_update.c ft_minimap.c ft_move.c ft_puts.c ft_texture.c ft_key_pressed.c -Lminilibx -lmlx -L/usr/X11/lib -lXext -lX11 -lm -lbsd -g
-#-lmlx -L/usr/X11/lib -lXext -lX11 -lm -g
-make fclean
-make
-make clean
-./cub3d maps/3.cub
